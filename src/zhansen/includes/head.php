428d833871004573c698950f43467e39388318d0
Derek.Chen <chingyuchen92@gmail.com>
2022-03-09 00:23:15 +0800

    update title icon

--- src/zhansen/includes/head.php
+++ src/zhansen/includes/head.php
@@ -20,11 +20,11 @@ function getHead($main_page, $add_path) {
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,600,400italic|Open+Sans:400,600,700' rel='stylesheet'>
 
     <!-- fav and touch icons -->
-    <link rel='shortcut icon' href='assets/ico/favicon.ico'>
-    <link rel='apple-touch-icon-precomposed' sizes='144x144' href='$add_path/assets/ico/apple-touch-icon-144-precomposed.png'>
-    <link rel='apple-touch-icon-precomposed' sizes='114x114' href='$add_path/assets/ico/apple-touch-icon-114-precomposed.png'>
-    <link rel='apple-touch-icon-precomposed' sizes='72x72' href='$add_path/assets/ico/apple-touch-icon-72-precomposed.png'>
-    <link rel='apple-touch-icon-precomposed' href='$add_path/assets/ico/apple-touch-icon-57-precomposed.png'>
+    <link rel='shortcut icon' href='$add_path/assets/ico/zhansen-favicon.ico'>
+    <link rel='apple-touch-icon-precomposed' sizes='144x144' href='$add_path/assets/ico/zhansen-144.png'>
+    <link rel='apple-touch-icon-precomposed' sizes='114x114' href='$add_path/assets/ico/zhansen-114.png'>
+    <link rel='apple-touch-icon-precomposed' sizes='72x72' href='$add_path/assets/ico/zhansen-72.png'>
+    <link rel='apple-touch-icon-precomposed' href='$add_path/assets/ico/zhansen-57.png'>
 
     <!-- =======================================================
     Theme Name: Lumia
